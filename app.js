823a4eb2282868284469efe90c2a6716d5978cb8
SamuelSub <samsubas01@gmail.com>
2020-10-06 14:45:32 +0300

    Fixed UI issues when selecting city from suggestions

--- app.js
+++ app.js
@@ -2,31 +2,29 @@ window.addEventListener('DOMContentLoaded', () => {
   ui.displayAfterRefresh(storage.get());
 });
 
-const searchCity = document.querySelector('#search-city');
+const searchCity = document.querySelector('#inputLarge');
 
 const weather = new Weather();
 const ui = new UI();
 const storage = new Store();
 
 
-
-searchCity.addEventListener('keyup', function(e) {
-  weather.getWeather(e.target.value)
-  .then(data => {
-    ui.displayWeather(data.main,data.name);
-    const saveLocally = new Store(data.main.temp,data.main.feels_like,data.main.humidity,data.name);
-    saveLocally.set();
-  })
-  .catch(err => console.log());
-  
-})
-
-
-
 let placesAutocomplete = places({
   appId: 'plOYZHPXK9IO',
   apiKey: '894ffa14b4bc2630622a1e4c50937c83',
   container: document.querySelector('#inputLarge')
+})
+
+// returns data if a suggestion is selected (more info in API Docs)
+placesAutocomplete.on('change', function resultSelected(e) {
+  weather.getWeather(e.suggestion.name)
+  .then(data => {
+    ui.displayWeather(data.main,data.name);
+    const weatherStorage = new Store(data.main.temp,data.main.feels_like,data.main.humidity,data.name);
+    weatherStorage.set();
+  })
+  .catch(err => console.log(err));
+  searchCity.value = '';
 });
 
 
